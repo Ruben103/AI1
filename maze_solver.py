bcd92676c0af245410813fff933199ddb22c692c
S2982129 <47632222+S2982129@users.noreply.github.com>
2019-04-30 16:06:05 +0200

    Add files via upload

--- maze_solver.py
+++ maze_solver.py
@@ -4,6 +4,17 @@ from maze import Maze, Room
 from fringe import Fringe
 from state import State
 
+def estimatedDistance(maze, room):
+	goal = maze.getGoal()
+	loc = room.coords
+	estCost = 0
+	for x in range(0,3):
+		dist = goal[x] - loc[x]
+		if x == 2 and dist:
+			dist *= 3 if dist > 0 else 2
+		estCost += dist ** 2
+	return estCost ** 0.5
+
 def solveMazeGeneral(maze, algorithm):
 	#select the right queue for each algorithm
 	if algorithm == "BFS":
@@ -19,13 +30,8 @@ def solveMazeGeneral(maze, algorithm):
 	goal = maze.getGoal()
 	room = maze.getRoom(*maze.getStart())
 	prio = 0
-	for x in range(0,3):
-		dist = goal[x]-room.coords[x]
-		if x == 2 and not dist:
-			dist *= 3 if goal[x] > room.coords[x] else 2
-		prio += (dist) ** 2
-	prio **= 0.5
-	state = State(room=room, parent=None, cost = 0, prio = prio)
+	prio = estimatedDistance(maze, room)
+	state = State(room = room, parent = None, cost = 0, prio = prio)
 	fr.push((prio, state))	
 	
 	#creates a list of all visited rooms
@@ -52,12 +58,9 @@ def solveMazeGeneral(maze, algorithm):
 			newRoom, cost = room.makeMove(d, state.getCost())
 			newState = State(newRoom, state, cost)
 			if algorithm == "GREEDY" or algorithm == "ASTAR":
-				cost = 0
-				for x in range(0,3):
-					cost += abs(goal[x]-newRoom.coords[x]) ** 2
-				cost **= 0.5
-				if algorithm == "ASTAR":
-					cost += state.cost
+				cost = estimatedDistance(maze, newRoom)
+			if algorithm == "ASTAR":
+				cost += state.cost if algorithm == "ASTAR" else 0
 			priority_tuple = (cost, newState)
 			#before pushing a new state, checks if it's in our list
 			if not str(newRoom.coords) in visited_rooms:
