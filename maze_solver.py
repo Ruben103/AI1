39b2e364a4ee136fb81e210079d98b14e7cb4dd8
S2982129 <47632222+S2982129@users.noreply.github.com>
2019-04-30 15:31:25 +0200

    Add files via upload

--- maze_solver.py
+++ maze_solver.py
@@ -10,24 +10,34 @@ def solveMazeGeneral(maze, algorithm):
 		fr = Fringe("FIFO")
 	elif algorithm == "DFS":
 		fr = Fringe("STACK")
-	elif algorithm == "UCS":
+	elif algorithm == "UCS" or algorithm == "GREEDY" or algorithm == "ASTAR":
 		fr = Fringe("PRIO")
 	else:
 		print("algorithm not found/implemented, exit")
 		return
 	
+	goal = maze.getGoal()
 	room = maze.getRoom(*maze.getStart())
-	state = State(room, None)
-	fr.push((0, state))	
+	prio = 0
+	for x in range(0,3):
+		dist = goal[x]-room.coords[x]
+		if x == 2 and not dist:
+			dist *= 3 if goal[x] > room.coords[x] else 2
+		prio += (dist) ** 2
+	prio **= 0.5
+	state = State(room=room, parent=None, cost = 0, prio = prio)
+	fr.push((prio, state))	
 	
 	#creates a list of all visited rooms
 	visited_rooms = [str(room.coords)]
 
 	while not fr.isEmpty():
 	
-		state = fr.pop()[1]
+		priority_tuple = fr.pop()
+		cost = priority_tuple[0]
+		state = priority_tuple[1]
 		room = state.getRoom()
-		print(str(room.coords))
+		print(str(room.coords)+ " " + str(cost))
 
 		if room.isGoal():
 			print("solved")
@@ -41,11 +51,18 @@ def solveMazeGeneral(maze, algorithm):
 			#get new room after move and cost to get there
 			newRoom, cost = room.makeMove(d, state.getCost())
 			newState = State(newRoom, state, cost)
+			if algorithm == "GREEDY" or algorithm == "ASTAR":
+				cost = 0
+				for x in range(0,3):
+					cost += abs(goal[x]-newRoom.coords[x]) ** 2
+				cost **= 0.5
+				if algorithm == "ASTAR":
+					cost += state.cost
 			priority_tuple = (cost, newState)
 			#before pushing a new state, checks if it's in our list
 			if not str(newRoom.coords) in visited_rooms:
 				visited_rooms.append(str(newRoom.coords))
-				fr.push((cost, newState))
+				fr.push(priority_tuple)
 
 	print("not solved")
 	fr.printStats()
